fac66224f34ac1c260a76841c0324f1130a606db
adipose <3324395+adipose@users.noreply.github.com>
2020-04-01 05:38:33 -0700

    show percent complete on OSD / status bar (#341)

--- src/mpc-hc/PlayerStatusBar.cpp
+++ src/mpc-hc/PlayerStatusBar.cpp
@@ -331,10 +331,21 @@ void CPlayerStatusBar::SetStatusTimer(REFERENCE_TIME rtNow, REFERENCE_TIME rtDur
         rstr.Format(_T("%I64d"), rtDur - rtNow);
     }
 
+    CString percentComplete;
     if (!s.fRemainingTime) {
-        str = ((rtDur <= 0) || (rtDur < rtNow)) ? posstr : posstr + _T(" / ") + durstr;
+        if ((rtDur <= 0) || (rtDur < rtNow)) {
+            str = posstr;
+        } else {
+            percentComplete.Format(_T(" [%3.2f%%]"), 100.f * (float(rtNow) / float(rtDur)));
+            str = posstr + _T(" / ") + durstr + percentComplete;
+        }
     } else {
-        str = ((rtDur <= 0) || (rtDur < rtNow)) ? posstr : _T("- ") + rstr + _T(" / ") + durstr;
+        if ((rtDur <= 0) || (rtDur < rtNow)) {
+            str = posstr;
+        } else {
+            percentComplete.Format(_T(" [%3.2f%%]"), 100.f * (float(rtDur - rtNow) / float(rtDur)));
+            str = _T("- ") + rstr + _T(" / ") + durstr + percentComplete;;
+        }
     }
 
     SetStatusTimer(str);
