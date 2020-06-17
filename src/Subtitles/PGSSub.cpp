c7cf4991603e0d575673f6e4d6101df7622a80a9
clsid2 <clsid2@users.sourceforge.net>
2020-06-17 16:11:32 +0200

    Catch file exception in pgssub

--- src/Subtitles/PGSSub.cpp
+++ src/Subtitles/PGSSub.cpp
@@ -534,15 +534,18 @@ bool CPGSSubFile::Open(CString fn, CString name /*= _T("")*/, CString videoName 
         m_name = name;
     }
 
-    CFile f;
-    if (f.Open(fn, CFile::modeRead | CFile::shareDenyWrite)) {
-        WORD wSyncCode = 0;
-        f.Read(&wSyncCode, sizeof(wSyncCode));
-        wSyncCode = _byteswap_ushort(wSyncCode);
-        if (wSyncCode == PGS_SYNC_CODE) {
-            m_parsingThread = std::thread([this, fn] { ParseFile(fn); });
-            bOpened = true;
+    try {
+        CFile f;
+        if (f.Open(fn, CFile::modeRead | CFile::shareDenyWrite)) {
+            WORD wSyncCode = 0;
+            f.Read(&wSyncCode, sizeof(wSyncCode));
+            wSyncCode = _byteswap_ushort(wSyncCode);
+            if (wSyncCode == PGS_SYNC_CODE) {
+                m_parsingThread = std::thread([this, fn] { ParseFile(fn); });
+                bOpened = true;
+            }
         }
+    } catch (CFileException*) {
     }
 
     return bOpened;
