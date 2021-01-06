8cfead1f277fb2125dc153292fcb9a9d7611b288
clsid2 <4704996+clsid2@users.noreply.github.com>
2021-01-06 20:29:49 +0100

    Small improvement in macrovision kicker code

--- src/filters/renderer/VideoRenderers/MacrovisionKicker.cpp
+++ src/filters/renderer/VideoRenderers/MacrovisionKicker.cpp
@@ -59,16 +59,13 @@ STDMETHODIMP CMacrovisionKicker::NonDelegatingQueryInterface(REFIID riid, void**
 
 STDMETHODIMP CMacrovisionKicker::Set(REFGUID PropSet, ULONG Id, LPVOID pInstanceData, ULONG InstanceLength, LPVOID pPropertyData, ULONG DataLength)
 {
+    if (PropSet == AM_KSPROPSETID_CopyProt && (Id == AM_PROPERTY_COPY_MACROVISION || Id == AM_PROPERTY_COPY_DIGITAL_CP)) {
+        TRACE(_T("Bypassing macrovision DVD protection\n"));
+        return S_OK;
+    }
     if (CComQIPtr<IKsPropertySet> pKsPS = m_pInner) {
-        if (PropSet == AM_KSPROPSETID_CopyProt && Id == AM_PROPERTY_COPY_MACROVISION
-                /*&& DataLength == 4 && *(DWORD*)pPropertyData*/) {
-            TRACE(_T("Oops, no-no-no, no macrovision please\n"));
-            return S_OK;
-        }
-
         return pKsPS->Set(PropSet, Id, pInstanceData, InstanceLength, pPropertyData, DataLength);
     }
-
     return E_UNEXPECTED;
 }
 
