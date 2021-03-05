d740a8e3ac9f47b67a1b5818f85acf8427f4ddde
adipose <3324395+adipose@users.noreply.github.com>
2021-03-05 03:36:05 -0800

    use utf-16 for rc file (#971)

--- src/mpc-hc/mpcresources/TranslationData.py
+++ src/mpc-hc/mpcresources/TranslationData.py
@@ -28,7 +28,7 @@ def xstr(s):
 
 
 def detectEncoding(filename):
-    encoding = 'utf-8'
+    encoding = 'utf_16_le'
 
     with codecs.open(filename, 'rb') as f:
         bytes = min(32, os.path.getsize(filename))
