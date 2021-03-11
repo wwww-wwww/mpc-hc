8a5d4b09299c12b19dcad61ffe25e5f325f5e0e0
adipose <3324395+adipose@users.noreply.github.com>
2021-03-11 04:53:27 -0800

    honor aspect ratio for preview window (#990)

--- src/mpc-hc/PlayerPreView.cpp
+++ src/mpc-hc/PlayerPreView.cpp
@@ -215,7 +215,9 @@ void CPreView::SetWindowSize() {
     // the preview size should not be larger than half size of the main window, but not less than 160
     w = std::max(160, std::min(w, wr.Width() / 2));
 
-    int h = (w - ((m_border + 1) * 2)) * 9 / 16;
+    CSize vs = m_pMainFrame->GetVideoSize();
+
+    int h = (w - ((m_border + 1) * 2)) * vs.cy / vs.cx;
     h += (m_caption + 1);
     h += (m_border + 1);
 
