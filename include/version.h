c334785a9d7ba1489c934d3973ca478bd9567700
clsid2 <4704996+clsid2@users.noreply.github.com>
2021-08-10 01:08:51 +0200

    1.9.15

--- include/version.h
+++ include/version.h
@@ -48,7 +48,7 @@
 
 #define MPC_VERSION_MAJOR       1
 #define MPC_VERSION_MINOR       9
-#define MPC_VERSION_PATCH       14
+#define MPC_VERSION_PATCH       15
 
 #if MPC_VERSION_REV > 0
 #define MPC_NIGHTLY_RELEASE     1
