3a47cec77890d8763bf97759e48260d85ccdfdff
clsid2 <4704996+clsid2@users.noreply.github.com>
2021-09-09 20:06:50 +0200

    Fix potential deadlock on EOF with MPCAR

--- src/thirdparty/MpcAudioRenderer/MpcAudioRenderer.cpp
+++ src/thirdparty/MpcAudioRenderer/MpcAudioRenderer.cpp
@@ -2949,7 +2949,7 @@ void CMpcAudioRenderer::WaitFinish()
 	for (;;) {
 		if (m_bNeedReinitialize || m_bNeedReinitializeFull
 				|| m_filterState == State_Stopped || !m_hRenderThread
-				|| !WasapiQueueSize()) {
+                || m_bFlushing || !WasapiQueueSize()) {
 			break;
 		}
 
