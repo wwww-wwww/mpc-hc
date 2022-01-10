ca362098fd0fffe42ed30ac516b25c1a14f5a1e6
clsid2 <4704996+clsid2@users.noreply.github.com>
2022-01-10 21:27:43 +0100

    Update libass and related libs

---
+++ src/thirdparty/fribidi/generated/fribidi-config.h
@@ -0,0 +1,24 @@
+/* fribidi-config.h.  Generated from fribidi-config.h.in by SMP. */
+/* Not copyrighted, in public domain. */
+#ifndef FRIBIDI_CONFIG_H
+#define FRIBIDI_CONFIG_H
+
+#define FRIBIDI "fribidi"
+#define FRIBIDI_NAME "GNU FriBidi"
+#define FRIBIDI_BUGREPORT "http://bugs.freedesktop.org/enter_bug.cgi?product=fribidi"
+
+#define FRIBIDI_VERSION "v1.0.11"
+#define FRIBIDI_MAJOR_VERSION 1
+#define FRIBIDI_MINOR_VERSION 0
+#define FRIBIDI_MICRO_VERSION 11
+#define FRIBIDI_INTERFACE_VERSION 4
+#define FRIBIDI_INTERFACE_VERSION_STRING "4"
+
+/* The size of a `int', as computed by sizeof. */
+#define FRIBIDI_SIZEOF_INT 4
+
+/* Define if fribidi was built with MSVC */
+
+#define FRIBIDI_LIB_STATIC
+
+#endif /* FRIBIDI_CONFIG_H */
