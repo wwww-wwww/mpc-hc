4a15c1a9c1340f4062fae5b7baa3ceaf854b2e7e
clsid2 <4704996+clsid2@users.noreply.github.com>
2022-02-13 00:00:43 +0100

    Simplify previous commit

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -1978,7 +1978,7 @@ void CMainFrame::OnTimer(UINT_PTR nIDEvent)
                                 return;
                             }
 
-                            if (m_bRememberFilePos && rtDur > 0LL && !m_fEndOfStream) {
+                            if (m_bRememberFilePos && !m_fEndOfStream) {
                                 auto* pMRU = &AfxGetAppSettings().MRU;
                                 pMRU->UpdateCurrentFilePosition(rtNow);
                             }
@@ -17407,14 +17407,10 @@ void CMainFrame::CloseMedia(bool bNextIsQueued/* = false*/)
     // save playback position
     if (GetLoadState() == MLS::LOADED) {
         auto& s = AfxGetAppSettings();
-        if (s.fKeepHistory && m_pMS) {
-            REFERENCE_TIME rtDur = 0;
-            m_pMS->GetDuration(&rtDur);
-            if (rtDur > 0) {
-                REFERENCE_TIME rtNow = 0;
-                m_pMS->GetCurrentPosition(&rtNow);
-                s.MRU.UpdateCurrentFilePosition(rtNow, true);
-            }
+        if (m_bRememberFilePos && m_pMS) {
+            REFERENCE_TIME rtNow = 0;
+            m_pMS->GetCurrentPosition(&rtNow);
+            s.MRU.UpdateCurrentFilePosition(rtNow, true);
         }
     }
 
