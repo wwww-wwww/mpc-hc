97c25d8209db7da5ce173e4e66ce05be48386164
clsid2 <4704996+clsid2@users.noreply.github.com>
2022-04-03 20:09:22 +0200

    dsf

--- src/mpc-hc/MediaFormats.cpp
+++ src/mpc-hc/MediaFormats.cpp
@@ -216,7 +216,7 @@ void CMediaFormats::UpdateData(bool fSave)
         ADDFMT((_T("wma"),         StrRes(IDS_MFMT_WMA),         _T("wma"), true));
         ADDFMT((_T("wavpack"),     StrRes(IDS_MFMT_WV),          _T("wv"), true));
         ADDFMT((_T("weba"),        _T("WebA"),                   _T("weba"), true));
-        ADDFMT((_T("other_audio"), StrRes(IDS_MFMT_OTHER_AUDIO), _T("aob mlp thd mpl spx caf"), true));
+        ADDFMT((_T("other_audio"), StrRes(IDS_MFMT_OTHER_AUDIO), _T("aob mlp thd mpl spx caf dsf"), true));
         ADDFMT((_T("pls"),         StrRes(IDS_MFMT_PLS),         _T("asx m3u m3u8 pls wvx wax wmx mpcpl")));
         ADDFMT((_T("cue"),         _T("Cue sheet"),              _T("cue")));
         ADDFMT((_T("bdpls"),       StrRes(IDS_MFMT_BDPLS),       _T("mpls bdmv")));
