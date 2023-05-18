f29c08ca57a66182a7d11dc8075ed973593257b6
adipose <3324395+adipose@users.noreply.github.com>
2023-05-18 07:59:06 -0700

    allow preview below seekbar (#2123)

--- src/mpc-hc/PlayerSeekBar.cpp
+++ src/mpc-hc/PlayerSeekBar.cpp
@@ -366,7 +366,14 @@ void CPlayerSeekBar::UpdateToolTipPosition(CPoint point)
         GetMonitorInfoW(MonitorFromWindow(m_hWnd, MONITOR_DEFAULTTONEAREST), &mi);
 
         point.x -= r_width / 2 - 2;
-        point.y = GetChannelRect().TopLeft().y - (r_height + 13);
+        CRect cRect = GetChannelRect();
+        CPoint bottomRight = cRect.BottomRight();
+        ClientToScreen(&bottomRight);
+        if (AfxGetAppSettings().nTimeTooltipPosition == TIME_TOOLTIP_BELOW_SEEKBAR && mi.rcWork.bottom > bottomRight.y + 20 + rc.Height()) {
+            point.y = cRect.BottomRight().y + 20;
+        } else {
+            point.y = cRect.TopLeft().y - (r_height + 13);
+        }
         ClientToScreen(&point);
         point.x = std::max(mi.rcWork.left + 5, std::min(point.x, mi.rcWork.right - r_width - 5));
 
