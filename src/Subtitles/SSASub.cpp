25a7a1c8e3cbf0f8808c17d05c7b7175d726f585
adipose <3324395+adipose@users.noreply.github.com>
2023-07-03 04:57:57 -0700

    libass sse2 patch (#2180)

--- src/Subtitles/SSASub.cpp
+++ src/Subtitles/SSASub.cpp
@@ -676,8 +676,153 @@ bool SSAUtil::RenderFrame(long long now, SubPicDesc& spd, CRect& rcDirty) {
     ASS_Image* image = ass_render_frame(m_renderer.get(), m_track.get(), now, &changed);
     if (!image) return false;
     if (changed)
-        AssFlatten(image, spd, rcDirty);
+        AssFlattenSSE2(image, spd, rcDirty);
     return true;
+}
+
+static __forceinline void pixmix_sse2(DWORD* dst, DWORD color, DWORD alpha)
+{
+    alpha = ((alpha + 1) * (color >> 24)) >> 8;
+    color &= 0xffffff;
+    __m128i zero = _mm_setzero_si128();
+    __m128i a = _mm_set1_epi32(((alpha + 1) << 16) | (0x100 - alpha));
+    __m128i d = _mm_unpacklo_epi8(_mm_cvtsi32_si128(*dst), zero);
+    __m128i s = _mm_unpacklo_epi8(_mm_cvtsi32_si128(color), zero);
+    __m128i r = _mm_unpacklo_epi16(d, s);
+    r = _mm_madd_epi16(r, a);
+    r = _mm_srli_epi32(r, 8);
+    r = _mm_packs_epi32(r, r);
+    r = _mm_packus_epi16(r, r);
+    *dst = (DWORD)_mm_cvtsi128_si32(r) + (alpha << 24);
+}
+
+static __forceinline __m128i packed_pix_mix_sse2(const __m128i& dst,
+    const __m128i& c_r, const __m128i& c_g, const __m128i& c_b, const __m128i& a) {
+    __m128i d_a, d_r, d_g, d_b;
+
+    d_a = _mm_srli_epi32(dst, 24);
+
+    d_r = _mm_slli_epi32(dst, 8);
+    d_r = _mm_srli_epi32(d_r, 24);
+
+    d_g = _mm_slli_epi32(dst, 16);
+    d_g = _mm_srli_epi32(d_g, 24);
+
+    d_b = _mm_slli_epi32(dst, 24);
+    d_b = _mm_srli_epi32(d_b, 24);
+
+    //d_a = _mm_or_si128(d_a, c_a);
+    d_r = _mm_or_si128(d_r, c_r);
+    d_g = _mm_or_si128(d_g, c_g);
+    d_b = _mm_or_si128(d_b, c_b);
+
+    d_a = _mm_mullo_epi16(d_a, a);
+    d_r = _mm_madd_epi16(d_r, a);
+    d_g = _mm_madd_epi16(d_g, a);
+    d_b = _mm_madd_epi16(d_b, a);
+
+    d_a = _mm_srli_epi32(d_a, 8);
+    d_r = _mm_srli_epi32(d_r, 8);
+    d_g = _mm_srli_epi32(d_g, 8);
+    d_b = _mm_srli_epi32(d_b, 8);
+
+    __m128i ones = _mm_set1_epi32(0x1);
+    __m128i a_sub_one = _mm_srli_epi32(a, 16);
+    a_sub_one = _mm_sub_epi32(a_sub_one, ones);
+    d_a = _mm_add_epi32(d_a, a_sub_one);
+
+    d_a = _mm_slli_epi32(d_a, 24);
+    d_r = _mm_slli_epi32(d_r, 16);
+    d_g = _mm_slli_epi32(d_g, 8);
+
+    d_b = _mm_or_si128(d_b, d_g);
+    d_b = _mm_or_si128(d_b, d_r);
+    return _mm_or_si128(d_b, d_a);
+}
+
+static __forceinline void packed_pix_mix_sse2(BYTE* dst, const BYTE* alpha, int w, DWORD color) {
+    __m128i c_r = _mm_set1_epi32((color & 0xFF0000));
+    __m128i c_g = _mm_set1_epi32((color & 0xFF00) << 8);
+    __m128i c_b = _mm_set1_epi32((color & 0xFF) << 16);
+    __m128i c_a = _mm_set1_epi16((color & 0xFF000000) >> 24);
+
+    __m128i zero = _mm_setzero_si128();
+
+    __m128i ones = _mm_set1_epi16(0x1);
+
+    const BYTE* alpha_end0 = alpha + (w & ~15);
+    const BYTE* alpha_end = alpha + w;
+    for (; alpha < alpha_end0; alpha += 16, dst += 16 * 4) {
+        __m128i a = _mm_loadu_si128(reinterpret_cast<const __m128i*>(alpha));
+        __m128i d1 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(dst));
+        __m128i d2 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(dst + 16));
+        __m128i d3 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(dst + 32));
+        __m128i d4 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(dst + 48));
+
+        __m128i ra = _mm_setzero_si128();
+        __m128i a1 = _mm_unpacklo_epi8(a, zero);
+        a1 = _mm_add_epi16(a1, ones);
+        a1 = _mm_mullo_epi16(a1, c_a);
+        a1 = _mm_srli_epi16(a1, 8);
+
+        __m128i a2 = _mm_unpackhi_epi8(a, zero);
+        a2 = _mm_add_epi16(a2, ones);
+        a2 = _mm_mullo_epi16(a2, c_a);
+        a2 = _mm_srli_epi16(a2, 8);
+
+        a = _mm_packus_epi16(a1, a2);
+
+        ra = _mm_cmpeq_epi32(ra, ra);
+        ra = _mm_xor_si128(ra, a);
+        a1 = _mm_unpacklo_epi8(ra, a);
+        a2 = _mm_unpackhi_epi8(a1, zero);
+        a1 = _mm_unpacklo_epi8(a1, zero);
+        a1 = _mm_add_epi16(a1, ones);
+        a2 = _mm_add_epi16(a2, ones);
+
+        __m128i a3 = _mm_unpackhi_epi8(ra, a);
+        __m128i a4 = _mm_unpackhi_epi8(a3, zero);
+        a3 = _mm_unpacklo_epi8(a3, zero);
+        a3 = _mm_add_epi16(a3, ones);
+        a4 = _mm_add_epi16(a4, ones);
+
+        d1 = packed_pix_mix_sse2(d1, c_r, c_g, c_b, a1);
+        d2 = packed_pix_mix_sse2(d2, c_r, c_g, c_b, a2);
+        d3 = packed_pix_mix_sse2(d3, c_r, c_g, c_b, a3);
+        d4 = packed_pix_mix_sse2(d4, c_r, c_g, c_b, a4);
+
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dst), d1);
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dst + 16), d2);
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dst + 32), d3);
+        _mm_storeu_si128(reinterpret_cast<__m128i*>(dst + 48), d4);
+    }
+    DWORD* dst_w = reinterpret_cast<DWORD*>(dst);
+    for (; alpha < alpha_end; alpha++, dst_w++) {
+        pixmix_sse2(dst_w, color, *alpha);
+    }
+}
+
+void SSAUtil::AssFlattenSSE2(ASS_Image* image, SubPicDesc& spd, CRect& rcDirty) {
+    if (image) {
+        CRect pRect;
+        for (auto i = image; i != nullptr; i = i->next) {
+            CRect rect2(i->dst_x, i->dst_y, i->dst_x + i->w, i->dst_y + i->h);
+            pRect.UnionRect(pRect, rect2);
+        }
+
+        CRect spdRect = GetSPDRect(spd);
+        rcDirty.IntersectRect(pRect + spdRect.TopLeft(), spdRect);
+
+        BYTE* pixelBytes = (BYTE*)(spd.bits + spd.pitch * rcDirty.top + rcDirty.left * 4);
+
+        for (auto i = image; i != nullptr; i = i->next) {
+            concurrency::parallel_for(0, i->h, [&](int y) {
+                BYTE* dst = &pixelBytes[((ptrdiff_t)i->dst_y + y - pRect.top) * spd.pitch + ((ptrdiff_t)i->dst_x - pRect.left) * 4];
+                auto alpha = &i->bitmap[y * i->stride];
+                packed_pix_mix_sse2(dst, alpha, i->w, (i->color >> 8) | (~(i->color) << 24));
+            }, concurrency::static_partitioner());
+        }
+    }
 }
 
 void SSAUtil::AssFlatten(ASS_Image* image, SubPicDesc& spd, CRect& rcDirty) {
--- src/Subtitles/SSASub.h
+++ src/Subtitles/SSASub.h
@@ -130,6 +130,7 @@ public:
     bool RenderFrame(long long now, SubPicDesc& spd, CRect& rcDirty);
     void SetFilterGraph(IFilterGraph* g) { m_pGraph = g; };
     void SetPin(IPin* i) { m_pPin = i; };
+    void AssFlattenSSE2(ASS_Image* imagee, SubPicDesc& spd, CRect& rcDirty);
     void AssFlatten(ASS_Image* image, SubPicDesc& spd, CRect& rcDirty);
     void SetFrameSize(int w, int h);
 protected:
